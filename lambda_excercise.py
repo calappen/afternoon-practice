e974f77ad3100592f3da92f8ea6f121c34082137
calappen <chrislappen@gmail.com>
2022-06-27 13:37:00 -0700

    in progress

--- lambda_excercise.py
+++ lambda_excercise.py
@@ -11,14 +11,27 @@ prog_lang = [('Python', 3.8),
 #1. Sort the list by each language's version in ascending order.
 #[('Scala', 2.13), ('Python', 3.8), ('Java', 13), ('JavaScript', 2019)]
 
+prog_lang.sort(key = lambda x: x[1])
+print(prog_lang)
+
 #2. Sort the list by the length of the name of each language in descending order.
 #[('JavaScript', 2019), ('Python', 3.8), ('Scala', 2.13), ('Java', 13)]
+
+prog_lang.sort(key = lambda x: len(x[0]), reverse=True)
+print(prog_lang)
 
 #3. Filter the list so that it only contains languages with 'a' in it.
 #[('Java', 13), ('JavaScript', 2019), ('Scala', 2.13)]
 
+prog_lang = list(filter(lambda x: ('a' in x[0]), prog_lang))
+prog_lang.sort(key = lambda x: x[0])
+print(prog_lang)
+
 #4. Filter the list so that it only contains languages whose version is in integer form.
 #[('Java', 13), ('JavaScript', 2019)]
+
+prog_lang = list(filter(lambda x: (isinstance(x[1],int)), prog_lang))
+print(prog_lang)
 
 #5. Transform the list so that it contains the tuples in the form,
 #("language in all lower case", length of the language string)
