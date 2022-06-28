9e4e8a62c309d219bceb90462fc0207aa473f71c
calappen <chrislappen@gmail.com>
2022-06-27 21:44:20 -0700

    solutions complete

--- generator_excercises.py
+++ generator_excercises.py
@@ -1,7 +1,19 @@
 #Generator Excercises
-
+import math
 #Problem 1
 #Create a generator, primes_gen that generates prime numbers starting from 2.
+
+def primes_gen():
+    n = 2
+    while True:
+        for i in range(2,int(math.sqrt(n))+1):
+            if n % i == 0:
+                break
+        else:
+            yield n
+        n += 1
+        
+        
 
 gen = primes_gen()
 for _ in range(10):
@@ -9,11 +21,21 @@ for _ in range(10):
 # Expected output
 # 2 3 5 7 11 13 17 19 23 29
 #----------------------------------------------------------------------------
-
+print()
 #Problem 2
 #Create a generator, unique_letters that generates unique letters from
 #the input string. It should generate the letters in the same order as
 #from the input string.
+
+def unique_letters(word):
+    n = 0
+    cleaned_word = ""
+    for letter in word:
+        if letter in word and letter not in cleaned_word:
+            cleaned_word += letter
+    while n < len(cleaned_word):
+        yield cleaned_word[n]
+        n += 1
 
 for letter in unique_letters('hello'):
     print(letter, end=' ')
--- lambda_excercise.py
+++ lambda_excercise.py
@@ -11,32 +11,40 @@ prog_lang = [('Python', 3.8),
 #1. Sort the list by each language's version in ascending order.
 #[('Scala', 2.13), ('Python', 3.8), ('Java', 13), ('JavaScript', 2019)]
 
-prog_lang.sort(key = lambda x: x[1])
-print(prog_lang)
+prog_lang_version = sorted(prog_lang, key = lambda x: x[1], reverse=False)
+print(prog_lang_version)
 
 #2. Sort the list by the length of the name of each language in descending order.
 #[('JavaScript', 2019), ('Python', 3.8), ('Scala', 2.13), ('Java', 13)]
 
-prog_lang.sort(key = lambda x: len(x[0]), reverse=True)
-print(prog_lang)
+prog_lang_name = sorted(prog_lang, key = lambda x: len(x[0]), reverse=True)
+print(prog_lang_name)
 
 #3. Filter the list so that it only contains languages with 'a' in it.
 #[('Java', 13), ('JavaScript', 2019), ('Scala', 2.13)]
 
-prog_lang = list(filter(lambda x: ('a' in x[0]), prog_lang))
-prog_lang.sort(key = lambda x: x[0])
-print(prog_lang)
+prog_lang_filter_a = list(filter(lambda x: ('a' in x[0]), prog_lang))
+prog_lang_filter_a.sort(key = lambda x: x[0])
+print(prog_lang_filter_a)
 
 #4. Filter the list so that it only contains languages whose version is in integer form.
 #[('Java', 13), ('JavaScript', 2019)]
 
-prog_lang = list(filter(lambda x: (isinstance(x[1],int)), prog_lang))
-print(prog_lang)
+prog_lang_filter_v = list(filter(lambda x: (isinstance(x[1],int)), prog_lang))
+print(prog_lang_filter_v)
 
 #5. Transform the list so that it contains the tuples in the form,
 #("language in all lower case", length of the language string)
 #[('python', 6), ('java', 4), ('javascript', 10), ('scala', 5)]
 
+prog_lang_lower = list(map(lambda x: (x[0].lower(),len(x[0])), prog_lang))
+print(prog_lang_lower)
+
 #6. Generate a tuple in the form,
 #("All languages separated by commas","All versions separated by commas")
 #('Python,Java,JavaScript,Scala', '3.8,13,2019,2.13')
+
+#prog_lang_tuple = ((list(map(lambda x: (x[0]), prog_lang))),list((map(lambda x: (x[1]), prog_lang))))                            
+
+prog_lang_tuple = (','.join(map(lambda x: (x[0]),prog_lang)),','.join(map(lambda x: str((x[1])),prog_lang)))
+print(prog_lang_tuple)                     
